1f6be644705ad46908f9a12ee1654815d6a1a1ad
Anas M.I <anas3rde@gmail.com>
2020-12-05 20:57:56 +0530

    added the logo

--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -11,6 +11,24 @@ left: 0;
 z-index: 99`
 
 export const Logo = styled.div`
+a{
+    font-size: 1.8rem;
+    font-weight: 800;
+    color: ${props => props.theme.text}
+}
+
+span{
+height: 1rem;
+width: 1rem;
+background: ${
+    props => props.theme.red
+};
+margin: 0 4px;
+border-radius: 100%;
+display: inline-block;
+position: relative;
+bottom: 2px;
+}
 
 `
 
