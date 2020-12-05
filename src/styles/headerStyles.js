c52000e4b170609374819b4d48a15f3fec51c92d
Anas M.I <anas3rde@gmail.com>
2020-12-05 21:20:35 +0530

    menu style

--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -34,6 +34,19 @@ bottom: 2px;
 
 
 export const Menu = styled.div`
-
+button{
+    transform-origin: center;
+    border: none;
+    padding: 20px;
+    background: none;
+    outline: none;
+    span{
+        width: 36px;
+        height: 8px;
+        display: block;
+        background: ${props => props.theme.text};
+        margin: 8px;
+    }
+}
 
 `
