166c88973cfe67c617c85d1e8f30877e90050aab
Anas M.I <anas3rde@gmail.com>
2020-12-06 14:14:48 +0530

    theme context

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,38 @@
-import React from "react"
-import {Link} from "gatsby"
+import React, { useEffect } from "react"
+import { Link } from "gatsby"
 // styled components
 import { HeaderNav, Logo, Menu } from "../styles/headerStyles"
 import { Container, Flex } from "../styles/globalStyles"
 
-//context provider 
-import {useGlobalStateContext} from "../context/globalContext"
+//context provider
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
 
 const Header = () => {
-    const {currentTheme}  = useGlobalStateContext()
-    return (
+  const dispatch = useGlobalDispatchContext()
+  const { currentTheme } = useGlobalStateContext()
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" })
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" })
+    }
+  }
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", currentTheme)
+  }, [currentTheme])
+  return (
     <HeaderNav>
-        {console.log(currentTheme)}
+      {console.log(currentTheme)}
       <Container>
         <Flex spaceBetween noHeight>
           <Logo>
-              <Link to="/">FURR</Link>
-              <span></span>
-              <Link to="/">W</Link>
-
+            <Link to="/">FURR</Link>
+            <span onClick={toggleTheme}></span>
+            <Link to="/">W</Link>
           </Logo>
           <Menu>
             <button>
