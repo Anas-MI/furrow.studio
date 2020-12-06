269045cb342e2a3a4eeddd438415e6a83e66e379
Anas M.I <anas3rde@gmail.com>
2020-12-06 14:15:10 +0530

    storage theme

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -30,8 +30,8 @@ position: relative;
 display: flex;
 align-items: center;
 
-${props => props.spaceBetween && css`justify-content: space-between`}
-${props => props.flexEnd && css`justify-content: flex-end`}
+${props => props.spaceBetween && css`justify-content: space-between;`}
+${props => props.flexEnd && css`justify-content: flex-end;`}
 ${props => props.alignTop && css`align-items: top;`}
 ${props => props.noHeight && css`height: 0;`}
 
